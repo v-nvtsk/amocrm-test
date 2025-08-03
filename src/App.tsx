5f3db33bf5229da4285e6633b07faea3960d186b
v-nvtsk <v@nvtsk.ru>
2025-08-03 20:07:35 +0300

    feat(qr): task complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { PasswordGenerator } from "./features/password/main";
+import { QRGenerator } from "./features/qr-generator/main";
 
 function App() {
 
   return (
     <>
+      <QRGenerator/>
       <PasswordGenerator/>
     </>
   );
